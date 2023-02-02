4f76d6b4941ecd9507584b622766e53a99134725
FranBudde <buddemeyerizzo@gmail.com>
2023-02-01 21:32:17 -0300

    nodemon y path arreglados

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const path = "path";
+const path = require("path");
 
 const publicPath = path.resolve(__dirname,'./public');
 app.use (express.static(publicPath));
