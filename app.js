db779b59199871922fbe41be04b130694b3e0895
FranBudde <buddemeyerizzo@gmail.com>
2023-02-08 01:33:10 -0300

    arregle las rutas de los html, y agregue la variable port

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ app.use (express.static(publicPath));
 
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/productCart.html")));
+app.get("/productCart", (req,res) => res.sendFile(path.resolve(__dirname,"./views/productCart.html")));
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/productDetail.html")));
+app.get("/productDetail", (req,res) => res.sendFile(path.resolve(__dirname,"./views/productDetail.html")));
 
-app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
 
 
-app.listen (3030,() => console.log("servidor http://localhost:3030/ abierto"));
+const port = process.env.PORT || 3030
+app.listen (port,() => console.log(`servidor http://localhost:${port}/ abierto`));
