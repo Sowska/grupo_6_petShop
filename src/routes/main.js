2a6a611808a5cd9229cf2da7de329b04c7bccb4b
FranBudde <buddemeyerizzo@gmail.com>
2023-03-07 14:59:57 -0300

    cambios aceptados de la rama main

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,10 +5,17 @@ const mainController = require('../controllers/mainController'); //ruta que nece
 const router = express.Router(); //ruta en comun
 
 router.get('/', mainController.index); //el metodo GET pide una vista
+
 router.get('/cart', mainController.cart);
+
 router.get('/createProduct',mainController.createProduct);
+
 router.get('/productDetail',mainController.productDetail);
+router.get('/:id',mainController.productDetail);
+
+
 router.get('/login',mainController.login);
+
 router.get('/register',mainController.register);
 
 module.exports = router; //ruta en comun
