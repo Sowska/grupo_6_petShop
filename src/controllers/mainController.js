cce87ed4f9e23974239e824eb728fea5528a8f8c
FranBudde <buddemeyerizzo@gmail.com>
2023-03-14 21:59:58 -0300

    terminado

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,9 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 
-function getProductos(){
-    return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-}
+
 
 const controller = {
     index: (req, res) => {
@@ -18,14 +16,6 @@ const controller = {
     createProduct: (req,res)=>{
     res.render('createProduct');
     },
-
-    // productDetail: (req,res)=>{
-    //     const {id} = req.params;
-    //     const productos = getProductos()
-    //     const product = productos.find((product) => product.id === +id)
-
-    //     res.render('productDetail',{product});
-    // },
 
     login: (req,res)=>{
         res.render('login');
