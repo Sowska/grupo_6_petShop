42bbb76280827a3e2a1610c11c2ef356e97719dc
Sowska <losowskisofia@gmail.com>
2023-03-24 11:35:48 -0300

    configuracion de rutas de usuarios

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -7,22 +7,6 @@ const controller = {
     
     cart: (req, res) => {
         res.render('cart');
-    }, 
-
-    createProduct: (req,res)=>{
-    res.render('createProduct');
-    },
-
-/*     productDetail: (req,res)=>{
-        res.render('productDetail');
-    }, */
-
-    login: (req,res)=>{
-        res.render('login');
-    },
-
-    register: (req,res)=>{
-        res.render('register');
     }
 }
 
