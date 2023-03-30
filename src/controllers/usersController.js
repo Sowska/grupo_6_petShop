8c44a2a80b541dff6e5ced9676555ee0bd16f6c2
Sowska <losowskisofia@gmail.com>
2023-03-29 21:07:12 -0300

    ruteo usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,12 @@
-const { validationResult } = require('express-validator');
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+
+function getUsers(){
+	return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+}
+
 
 const controller = {
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,19 @@ const usersController = require('../controllers/usersController');
 
 const router = express.Router();
 
+//Formulario de ingreso
 router.get('/login',usersController.login);
+
+//Formulario de creación
 router.get('/register',usersController.register);
+
+//Procesamiento del formulario de creacion
+router.post('/', validateUserForm, usersController.store);
+
+//Detalle de un Usuario
+
+
+//Todos los usuarios
 
 
 
