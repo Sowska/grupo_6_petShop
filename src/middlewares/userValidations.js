284e4b6564a2d8171f33395b5a4f14ada3944c65
FranBudde <buddemeyerizzo@gmail.com>
2023-04-09 20:19:52 -0300

    validacioens de minimo de caracteres listo

--- src/middlewares/userValidations.js
+++ src/middlewares/userValidations.js
@@ -3,13 +3,17 @@ const express = require('express');
 
 
 const validatorCreateForm = [
-    body('firstName').notEmpty().withMessage('Tienes que escribir un nombre'),
+    body('firstName').notEmpty().withMessage('Tienes que escribir un nombre')
+    .isLength({ min: 5 }).withMessage('minimo tiene que tener 5 caracteres'),
 
-    body('lastName').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('lastName').notEmpty().withMessage('Tienes que escribir un apellido')
+    .isLength({ min: 5 }).withMessage('minimo tiene que tener 5 caracteres'),
 
     body('email').notEmpty().withMessage('tiene que escribir un email').bail()
-    .isEmail().withMessage('Tienes que escribir un formato de email').bail(),
-    body('password').notEmpty().withMessage('ingresa una contraseña')
+    .isEmail().withMessage('Tienes que escribir un formato de email'),
+
+    body('password').notEmpty().withMessage('ingresa una contraseña').bail()
+    .isLength({ min: 8 }).withMessage('minimo tiene que tener 8 caracteres')
 
 ];
 
